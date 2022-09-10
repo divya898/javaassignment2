842fdf4bbe198f5bee4feccf68798afafcc758a3
divya898 <112712310+divya898@users.noreply.github.com>
2022-09-10 19:43:57 +0530

    Update ass14.java

--- ass14.java
+++ ass14.java
@@ -6,12 +6,10 @@
 import java.util.Scanner;  
 public class ass14 
 {  
-//method for reverse a number  
 public static void reverseNumber(int number)   
 {  
 if (number < 10)   
-{  
-//prints the same number if the number is less than 10  
+{   
 System.out.println(number);  
 return;  
 }  
@@ -27,7 +25,6 @@ System.out.print("Enter the number that you want to reverse: ");
 Scanner sc = new Scanner(System.in);  
 int num = sc.nextInt();  
 System.out.print("The reverse of the given number is: ");  
-//method calling  
 reverseNumber(num);  
 }  
 }  
